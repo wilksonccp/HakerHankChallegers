3c07b978839ea9e3e56732409b35e7e92c5f1b48
Wilkson Pedrosa <wilkson.cc@gmail.com>
2025-05-07 21:31:38 -0300

    primeiro dia

---
+++ ProblemSolving/ProblemSolving/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+class Result
+{
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int n = arr.Count();
+        int rightD =0;
+        int leftD =0;
+
+        foreach (var row in arr)
+        {
+            if (row.Count != n)
+            {
+                throw new ArgumentException("Matrix is not square!");
+            }
+        }
+
+        for (int i = 0 ; i < n; i++ )
+        {
+            rightD += arr[i][i];
+            leftD += arr[i][n -1 -i];
+        }
+
+        return Math.Abs(leftD - rightD);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ ProblemSolving/ProblemSolving02/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        int n = arr.Count();
+        int pos = 0;
+        int neg = 0;
+        int zero = 0;
+
+        for (int i = 0; i<n; i++)
+        {
+            if (arr[i] > 0)
+            {
+                pos++;
+            }
+            else if (arr[i] < 0)
+            {
+                neg++;
+            }
+            else
+            {
+                zero++;
+            }
+        }
+            Console.WriteLine($"{(double)pos / n:f6}");
+            Console.WriteLine($"{(double)neg / n:f6}");
+            Console.WriteLine($"{(double)zero / n:f6}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
