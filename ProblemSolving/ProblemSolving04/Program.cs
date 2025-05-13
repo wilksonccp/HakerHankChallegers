09746e719d798f3be3d9d0776392597243be4437
Wilkson Pedrosa <wilkson.cc@gmail.com>
2025-05-12 22:05:01 -0300

    finalizado segundo dia de exercicios

---
+++ ProblemSolving/ProblemSolving04/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+public static void miniMaxSum(List<int> arr)
+{
+    long totalSum = arr.Sum(x => (long)x); 
+    int min = arr.Min();
+    int max = arr.Max();
+
+    long minSum = totalSum - max;
+    long maxSum = totalSum - min;
+
+    Console.WriteLine($"{minSum} {maxSum}");
+}
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
